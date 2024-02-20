3fd43c332cb08aaff62d0a876b4a259a0e366b5d
Tim Deeb-Swihart <tim.deebswihart@temporal.io>
2024-02-20 11:16:03 -0800

    Ban the connect attr interpolateParams for MySQL 8 Vis dbs
    There are a number of reasons why this doesn't work right now so we're
    going to prevent it from happening while we discuss whether we should
    invest in fixing it. The current work to fix this is in https://github.com/temporalio/temporal/pull/5428

--- common/persistence/sql/sqlplugin/mysql/plugin_v8.go
+++ common/persistence/sql/sqlplugin/mysql/plugin_v8.go
@@ -28,6 +28,7 @@ import (
 	"go.temporal.io/server/common/config"
 	"go.temporal.io/server/common/persistence/sql"
 	"go.temporal.io/server/common/persistence/sql/sqlplugin"
+	"go.temporal.io/server/common/persistence/sql/sqlplugin/mysql/session"
 	"go.temporal.io/server/common/resolver"
 )
 
@@ -52,7 +53,7 @@ func (p *pluginV8) CreateDB(
 	cfg *config.SQL,
 	r resolver.ServiceResolver,
 ) (sqlplugin.DB, error) {
-	conn, err := p.createDBConnection(cfg, r)
+	conn, err := p.createDBConnection(session.MySQLVersion8_0, dbKind, cfg, r)
 	if err != nil {
 		return nil, err
 	}
@@ -66,7 +67,7 @@ func (p *pluginV8) CreateAdminDB(
 	cfg *config.SQL,
 	r resolver.ServiceResolver,
 ) (sqlplugin.AdminDB, error) {
-	conn, err := p.createDBConnection(cfg, r)
+	conn, err := p.createDBConnection(session.MySQLVersion8_0, sqlplugin.DbKindMain, cfg, r)
 	if err != nil {
 		return nil, err
 	}
